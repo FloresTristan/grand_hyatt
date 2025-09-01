2bff68fa333f3eea4f78d0d8be8dc3ebf14e1838
FloresTristan <michaeltristanflores646@gmail.com>
2025-09-01 15:37:40 +0800

    .

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nDsr3MZ2FaIMXO1ZUwnSPN/mD74IVQruDq0vfoUZuGM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
