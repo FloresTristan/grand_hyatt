091601adde7a5b4f1eb9c6cb819b49e5145bd356
FloresTristan <michaeltristanflores646@gmail.com>
2025-09-09 13:03:19 +0800

    rebuild

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8lswYRbT7iAiqZ0VYctv2CF1KaeTKcg5Uti35BsCDxM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
