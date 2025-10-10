7f495a019947429f94c2b73f12e9a73b3e7d9cf9
FloresTristan <michaeltristanflores646@gmail.com>
2025-10-10 11:41:52 +0800

    rebuild

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="mobile-web-app-capable" content="yes" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
